79e6d648123b749a105f3f42b7c8d8fcac0d5270
AustinBeal <austinbeal89@gmail.com>
2020-09-28 18:43:21 -0500

    did assigned promts

---
+++ conditionals.js
@@ -0,0 +1,67 @@
+//let grade = 90;
+
+
+//if (grade > 90){
+//console.log(thats a nice A);
+//} else {
+//console.log(better luck next time);
+//}
+
+//console.log('your grade is:', grade);
+// i want some code that wil check if the code is above 90 or not
+// if its above, pring thats a nice A
+// otherwise print better luck next time 
+//let hungry = 'very' //little , no
+//if(hungry = "very"){
+//console.log('you should have a full size meal')
+//} else if (hungry = little){
+//console.log('grab a snack')
+//} else if (hungry = no){
+//console.log('sounds good, save it for later')
+//}
+
+
+// 
+// let color = 'red'; //red blue purp
+// let type ; // sports truck sedan suv
+
+// //will print if 
+// //red, yes\
+// if (color == "red"){
+//   console.log(YES PLZ)
+// }
+// //sports, yes
+// if (type == "sports"){
+//   console.log(YES PLZ)
+// }
+// //blue sedan
+// if ((color == "blue") && (type = "sedan")){
+//   console.log(YES PLZ)
+// }
+// //purp suv
+// if ((color == "purple") && (type == "suv")){
+//   console.log(YES PLZ)
+// }
+// //if white no
+// if (color == "white"){
+//   console.log(not interested)
+// }
+// //if sedan that is anything other than black or blue, no
+// if ((type == "sedan") && (color != "black" && "blue")){
+//   console.log(not interested)
+// }
+// //if it is an suv that is not purple , no
+// if ((type == "suv") && (color != "purple")){
+//   console.log(not interested)
+// }
+// //everything else maybe
+// if else {
+//   console.log("maybe")
+// }
+
+let echo = function(input){
+  return input;
+}
+let x = echo(7)
+
+console.log(x)
--- main.js
+++ main.js
@@ -5,21 +5,56 @@
 //          PART ONE
 // ***************************
 // Write a JavaScript program to display the current day and time, start with:
-console.log(new Date)
+
 
 const displayDate = () => {
   const currentDate = new Date()
-
   document.getElementById("display-element").innerHTML = currentDate;
 }
  
 
+
+
+
 // Write a JavaScript program to convert a number to a string.
+
+let numberExample = 100
+let stringExample = '99'
+let numberOFInput = document.getElementById("NumberInHere");
+
+
+let idNumber = 1;
+let id = 1
+
+let setIdSearch = (id) => {
+    idNumber = id
+    console.log(idNumber)
+  }
+
+const numberToString = () => {
+  makeString = idNumber.toString()
+  console.log('our new string is', makeString)
+  
+}
+
+numberToString(numberExample)
 
 
 
 // Write a JavaScript program to convert a string to the number.
+let idNumber2 = '';
+let id2 = ''
 
+let setIdSearch2 = (id2) => {
+    idNumber2 = id2
+    console.log(idNumber2)
+  }
+
+const stringToNumber = () => {
+  makeNumber = parseInt(idNumber2, 10)
+  console.log('our new number is', makeNumber)
+}
+stringToNumber(stringExample)
 
 
 // Write a JavaScript program that takes in different datatypes and prints out whether they are a:
@@ -30,21 +65,68 @@ const displayDate = () => {
   // * NaN
   // * String
   
+  let booleanVar = true;
 
+  let noValue = null;
+  
+  let undefinedVar;
+  
+  let numberVariable = 1;
+  
+  let stringVar = 'This is a String'
+  
+  const checkVariableType = (checkVariable) => {
+    typeof checkVariable
+    console.log(typeof checkVariable)
+  }
+  checkVariableType(stringVar)
+  // console.log('below are checked data types')
+  // checkVariableType(booleanVar)
+  // checkVariableType(noValue)
+  // checkVariableType(undefinedVar)
+  // checkVariableType(numberVariable)
+  // checkVariableType(stringVar)
+  
   
 // Write a JavaScript program that adds 2 numbers together.
+var num1 = 30
+var num2 = 25
 
+const addNums = (x, y) => {
+ sum = x + y;
+ console.log(sum)
+}
+ addNums(num1, num2)
 
 
 // Write a JavaScript program that runs only when 2 things are true.
+var trueCheck1 = true
+var trueCheck2 = true
+var falseCheck1 = false
+var falseCheck2 = false
 
-
+const checkTruthliness = (booleanValue1, booleanValue2) => {
+  if((booleanValue1 === true) && (booleanValue2 === true)){
+    console.log('its true! twice')}
+}
+checkTruthliness(trueCheck1, trueCheck2)
 
 // Write a JavaScript program that runs when 1 of 2 things are true.
+const checkHalfTruthliness = (booleanValue1, booleanValue2) => {
+  if((booleanValue1 === true) && (booleanValue2 === false))
+    console.log('its half true!');
+}
+checkHalfTruthliness(trueCheck1, falseCheck2)
 
 
 
 // Write a JavaScript program that runs when both things are not true.  
+const checkFalseliness = (booleanValue1, booleanValue2) => {
+  if((booleanValue1 === false) && (booleanValue2 === false))
+    console.log('its totally false!');
+}
+checkFalseliness(falseCheck1, falseCheck2)
+
 
 // ***************************
 //         PART TWO
@@ -65,3 +147,5 @@ const displayDate = () => {
 // Video1: https://player.vimeo.com/video/377147232
 // Video2: https://www.youtube.com/embed/bkvH28PXLWc
 // Video3: https://www.youtube.com/embed/TrGI9Yki-24
+
+
